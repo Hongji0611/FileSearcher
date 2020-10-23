a366be022d9de9462cb66ef91e464b57796d90dc
KangDroid <hyunwoo286@gmail.com>
2020-10-23 14:20:06 +0900

    Change strings

--- Parser.h
+++ Parser.h
@@ -28,12 +28,12 @@ public:
     void split(string key, string value);
     bool check_datapath(string value);
     void help(){
-        cout <<"HELP: $ program_name --to_find=”some_strings..” --directory=”to_search” --type=”grep|file_name|directory_name” --save_output=”to_save_directory” --file_verbose=”true|false”\n"<<endl;
-        cout<<"--directory=”to_search”    Search basis directory recognized by strings, can be absolute directory or relative directory. If this isn’t defined, program will set directory value to program-called directory as default.\n"<<endl;
-        cout<<"--to_find=”some_strings”    Things to search, could be any strings including ‘?’, ‘*’ wildcards. If this isn’t defined, program will throw error and exit.\n"<<endl;
-        cout<<"--type=”grep|file_name|directory_name”    The search type. Grep will search every file containing specific string[which is defined on --to_find]. If this value isn’t defined, program will set type to file_name as default.\n"<<endl;
-        cout<<"--save_output=”to_save_directory”    The file path to redirect STDOUT, can be absolute/relative path. If this isn’t defined, program will not redirect output to file.\n"<<endl;
-        cout<<"--file_verbose=”true|false”    Whether to see output as verbose output, only accepting “true” or “false”. If this isn’t defined, program will set --file_verbose to false as default.\n"<<endl;
-        cout<<"eg. $ among_file --to_find=”what” --directory=”/” --type=”grep” --save_output=”/Users/kangdroid/otp.txt” --file_verbose=”false"<<endl;
+        cout <<"HELP: $ program_name --to_find=\"some_strings..\" --directory=\"to_search\" --type=\"grep|file_name|directory_name\" --save_output=\"to_save_directory\" --file_verbose=\"true|false\"\n"<<endl;
+        cout<<"--directory=\"to_search\"    Search basis directory recognized by strings, can be absolute directory or relative directory. If this isn't defined, program will set directory value to program-called directory as default.\n"<<endl;
+        cout<<"--to_find=\"some_strings\"    Things to search, could be any strings including '?', '*' wildcards. If this isn't defined, program will throw error and exit.\n"<<endl;
+        cout<<"--type=\"grep|file_name|directory_name\"    The search type. Grep will search every file containing specific string[which is defined on --to_find]. If this value isn't defined, program will set type to file_name as default.\n"<<endl;
+        cout<<"--save_output=\"to_save_directory\"    The file path to redirect STDOUT, can be absolute/relative path. If this isn't defined, program will not redirect output to file.\n"<<endl;
+        cout<<"--file_verbose=\"true|false\"    Whether to see output as verbose output, only accepting \"true\" or \"false\". If this isn't defined, program will set --file_verbose to false as default.\n"<<endl;
+        cout<<"eg. $ among_file --to_find=\"what\" --directory=\"/\" --type=\"grep\" --save_output=\"/Users/kangdroid/otp.txt\" --file_verbose=\"false\""<<endl;
     }
 };
